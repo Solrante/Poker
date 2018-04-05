fe22d0541b805f6c27066ba045f8d29182601815
Solrante <aecm.cristianrodriguez@gmail.com>
2018-04-05 16:22:00 +0200

    Añadido proyecto de Sandcastle para generación de documentación del proyecto del cliente.
    Documentada y comentada de manera integra la clase "Login" del cliente.
    
    Signed-off-by: Solrante <aecm.cristianrodriguez@gmail.com>

--- Cliente Poker/Login.cs
+++ Cliente Poker/Login.cs
@@ -10,27 +10,50 @@ using System.Windows.Forms;
 
 namespace Cliente_Poker
 {
+    /// <summary>
+    /// Formulario para el acceso al servicio online
+    /// </summary>
+    /// <seealso cref="System.Windows.Forms.Form" />
     public partial class Login : Form
     {
+        /// <summary>
+        /// Contiene el resultado de comprobar las credenciales introducidas
+        /// </summary>
         public bool loginValido = false;
+        /// <summary>
+        /// Variable númerica que contendra el valor en pixel del centro del formulario
+        /// </summary>
         private int centroHorizontal;
+        /// <summary>
+        /// Instancia de la clase gestora con la conexion al servidor
+        /// </summary>
         private ConexionServidor conexion;
 
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="Login"/> .
+        /// </summary>
+        /// <param name="conexion">Instancia del gestor de conexion.</param>
         public Login(ConexionServidor conexion)
         {
             InitializeComponent();
             this.conexion = conexion;
         }
-
+        /// <summary>
+        /// Gestiona el evento Click del control btnEntrar.
+        /// </summary>
+        /// <param name="sender">El origen del evento.</param>
+        /// <param name="e">La instancia <see cref="EventArgs"/> contenedora de la información del evento.</param>
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-            string credenciales;
-            credenciales = txtCorreo.Text + "," + txtContraseña.Text;
-            Console.WriteLine("Mando a la conexion las credenciales");
+            //Se generá el valor a enviar como datos de login según el contenido de los controles siguiendo
+            //el formato establecido en el protocolo.
+            string credenciales = txtCorreo.Text + "," + txtContraseña.Text;
+            //Se abre y se mandan dichos datos a través de la conexión al servidor
             conexion.abrirConexion();
             conexion.enviarMensaje(credenciales);
-            string respuesta = conexion.recibirMensaje();
-            if (respuesta == "Login - Invalido")
+            //Se comprueba la respuesta del servidor a las credenciales mandadas , avisando de información erronea en la misma
+            //o accediendo al sistema de ser correcta.
+            if (conexion.recibirMensaje() == "Login - Invalido")
             {
                 lblError.Visible = true;
             }
@@ -41,9 +64,14 @@ namespace Cliente_Poker
             }
 
         }
-
+        /// <summary>
+        /// Gestiona el evento Load del formulario.
+        /// </summary>
+        /// <param name="sender">El origen del evento.</param>
+        /// <param name="e">La instancia <see cref="EventArgs"/> contenedora de la información del evento.</param>
         private void Login_Load(object sender, EventArgs e)
         {
+            //Centro todos los elementos al medio de manera horizontal del formulario según su tamaño.
             centroHorizontal = ClientSize.Width / 2;
             lblCorreo.Location = new Point(centroHorizontal - lblCorreo.Width / 2, lblCorreo.Location.Y);
             lblContraseña.Location = new Point(centroHorizontal - lblContraseña.Width / 2, lblContraseña.Location.Y);
